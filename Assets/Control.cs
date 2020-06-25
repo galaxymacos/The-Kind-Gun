c53f4364f3e34e850f22a3ec96e11ad45681d5c1
Xun Ruan <galaxymacos@gmail.com>
2020-06-24 22:44:17 -0400

    Initial commit

---
+++ Assets/Control.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using LuaInterface;
+public class Control : MonoBehaviour {
+    LuaState lua = null;
+    LuaFunction luaFunc = null;
+    void Start () {
+        new LuaResLoader();
+        lua = new LuaState();
+        lua.Start();
+        LuaBinder.Bind(lua);
+        string luaPath = Application.dataPath + "/Scripts/Lua";//注意这里的文件位置
+        lua.AddSearchPath(luaPath);
+        lua.DoFile("Control.lua");
+        CallFunc("Control.Start", gameObject);//调用lua中的this.Start函数
+    }
+    void Update () {
+        CallFunc("Control.Update", gameObject);////调用lua中的this.Update函数
+    }
+    private void OnApplicationQuit()
+    {
+        lua.Dispose();
+        lua = null;
+    }
+    void CallFunc(string func, GameObject obj){
+        luaFunc = lua.GetFunction(func);
+        luaFunc.Call(obj);
+        luaFunc.Dispose();
+        luaFunc = null;
+    }
+}
---
+++ Assets/Scripts/Lua/Control.lua
@@ -0,0 +1,27 @@
+﻿Control = {}
+local this = Control
+require('Music')
+local GameObject = UnityEngine.GameObject
+local Input = UnityEngine.Input
+local AudioSource = UnityEngine.AudioSource
+local Rigidbody = UnityEngine.Rigidbody
+local Color = UnityEngine.Color
+local Sphere
+local rigi
+local force
+
+function this.Start()
+    Sphere = GameObject.Find('Sphere')
+    Sphere : GetComponent('Renderer').material.color = Color(1, 0.1, 1)
+    Sphere : AddComponent(typeof(AudioSource))
+    coroutine.start(Music.PlaySound)
+    Sphere : AddComponent(typeof(Rigidbody))
+    rigi = Sphere : GetComponent('Rigidbody')
+    force = 5
+end
+
+function this.Update()
+    local h = Input.GetAxis('Horizontal')
+    local v = Input.GetAxis('Vertical')
+    rigi : AddForce(Vector3(h, 0, v) * force)
+end 
